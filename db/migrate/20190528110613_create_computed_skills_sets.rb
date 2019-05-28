8a1012090024628f3413ed6d100792672261e87a
Edouard Garret <edouard.garret@gmail.com>
2019-05-28 15:11:03 +0200

    create ComputedSkillsSet model

--- db/migrate/20190528110613_create_computed_skills_sets.rb
+++ db/migrate/20190528110613_create_computed_skills_sets.rb
@@ -2,14 +2,14 @@ class CreateComputedSkillsSets < ActiveRecord::Migration[5.2]
   def change
     create_table :computed_skills_sets do |t|
       t.references :user, foreign_key: true
-      t.integer :serve
-      t.integer :return
-      t.integer :backhand
-      t.integer :forehand
-      t.integer :volley
-      t.integer :speed
-      t.integer :power
-      t.integer :endurance
+      t.float :serve, default: 5
+      t.float :return, default: 5
+      t.float :backhand, default: 5
+      t.float :forehand, default: 5
+      t.float :volley, default: 5
+      t.float :speed, default: 5
+      t.float :power, default: 5
+      t.float :endurance, default: 5
 
       t.timestamps
     end
