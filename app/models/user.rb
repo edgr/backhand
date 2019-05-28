64e27c1b8a79723952cb4d4aa05d1339de245cc6
Edouard Garret <edouard.garret@gmail.com>
2019-05-28 12:30:54 +0200

    create challenge model with validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :sent_challenges, class_name: 'Challenge'
+  has_many :received_challenges, class_name: 'Challenge'
 end
