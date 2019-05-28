a6a9c004489d2effe8ebd354958d4e0fbefb38b0
Edouard Garret <edouard.garret@gmail.com>
2019-05-28 16:16:38 +0200

    Create index and show routes with corresponding controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   devise_for :users
+  resources :users, only: [ :index, :show, :edit, :update ]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
