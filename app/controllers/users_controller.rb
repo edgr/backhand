69f053ca9c6d125d210280604483c4cfef1994ef
Edouard Garret <edouard.garret@gmail.com>
2019-06-01 16:35:28 +0200

    fix empty search to display all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,13 @@ class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    if params[:search].present?
+    if params[:search].delete("").empty?
+      @users = User.all
+    elsif params[:search].present?
       params[:search].delete("")
       @users = User.search_user_fields(params[:search])
-    else
-      @users = User.all
     end
+    return @users
   end
 
   def show
