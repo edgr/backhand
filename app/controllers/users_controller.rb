aa3aebe0692aeabb20bfb533bb20226d39922688
Edouard Garret <edouard.garret@gmail.com>
2019-06-01 17:25:51 +0200

    fix blank search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,13 @@ class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    if params[:search].delete("").empty?
+    # raise
+    if params[:search].reject(&:blank?).empty?
       @users = User.all
     elsif params[:search].present?
       params[:search].delete("")
       @users = User.search_user_fields(params[:search])
     end
-    return @users
   end
 
   def show
