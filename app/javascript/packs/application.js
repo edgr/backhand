042d30833ee2a887c4ed201a839d0bf262fc68b4
Edouard Garret <edouard.garret@gmail.com>
2019-06-06 00:12:02 +0200

    added datepicker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,7 @@ import "bootstrap";
 import { addListener } from "../components/tabs";
 import { initAutocompleteAddress } from '../plugins/init_autocomplete';
 import { initAutocompleteCity } from '../plugins/autocomplete_city';
+import "../plugins/flatpickr";
 
 const tabs = document.querySelectorAll(".tab-underlined");
 if (tabs) { tabs.forEach(addListener) };
