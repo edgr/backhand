6b6dfbddc711f5259b5b2db1c335134a719df6fc
Leonard Siebold <leo.siebold@hotmail.de>
2019-06-05 12:12:35 +0200

    implement ranking

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,24 @@ class UsersController < ApplicationController
     else
       @users = User.all
     end
+    update_ranking
   end
 
   def show
     @user = User.find(params[:id])
     @reviews = UserReview.where(receiver_id: @user.id)
+  end
+
+  private
+
+  def update_ranking
+    @users = User.all
+    @users = @users.sort_by { |user| user.points }
+    counter = 1
+    @users.each do |user|
+      user.update(ranking: counter)
+      user.save
+      counter += 1
+    end
   end
 end
