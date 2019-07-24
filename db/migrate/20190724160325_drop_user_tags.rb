bfaf47667dfc291377de128bf19465864f67680b
Edouard Garret <edouard.garret@gmail.com>
2019-07-24 18:17:35 +0200

    removing foreign before migrating

--- db/migrate/20190724160325_drop_user_tags.rb
+++ db/migrate/20190724160325_drop_user_tags.rb
@@ -1,5 +1,11 @@
 class DropUserTags < ActiveRecord::Migration[5.2]
   def change
+    if foreign_key_exists?(:user_tags, :tags)
+      remove_foreign_key :user_tags, :tags
+    end
+    if foreign_key_exists?(:user_tags, :users)
+      remove_foreign_key :user_tags, :users
+    end
     drop_table :user_tags
   end
 end
