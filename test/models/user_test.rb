5314dd1df4dc6ce2ccdf54946d620d124390c10f
Edouard Garret <edouard.garret@gmail.com>
2019-07-25 11:23:49 +0200

    adding first simple tests to user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "full_name returns the capitalized first name and last name" do
+    user = User.new(first_name: "john", last_name: "lennon")
+    assert_equal "John Lennon", user.full_name
+  end
+  test "full_name returns the capitalized & downcased first name and last name" do
+    user = User.new(first_name: "JOHN", last_name: "leNNon")
+    assert_equal "John Lennon", user.full_name
+  end
+  test "age should be accurate" do
+    user = User.new(birthday: "2007-07-24")
+    assert_equal 12, user.age
+  end
+  test "age should not break if given today's date" do
+    user = User.new(birthday: Date.today)
+    assert_equal 0, user.age
+  end
 end
