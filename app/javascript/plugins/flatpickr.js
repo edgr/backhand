098ccd39512aa9f32725c80a8f738f3addee875c
Florin Diconescu <florin.diconescu@gmail.com>
2019-07-27 04:19:39 +0300

    adding calendarpicker

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,7 +1,10 @@
-import flatpickr from "flatpickr"
-import "flatpickr/dist/flatpickr.min.css" // Note this is important!
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/themes/material_blue.css';
 
 flatpickr(".datepicker", {
   altInput: true,
-  disableMobile: true
+  disableMobile: true,
+  altFormat: "j F Y",
+  dateFormat: "d-m-Y",
+  defaultDate: "16-07-2007"
 })
