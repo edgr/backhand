d95bdaefa21a4d15c5f4578523ae28ede9c028df
Edouard Garret <edouard.garret@gmail.com>
2019-07-27 16:31:44 +0200

    removing yarn package

--- app/javascript/components/init_select2.js
+++ app/javascript/components/init_select2.js
@@ -2,7 +2,9 @@ import $ from 'jquery';
 import 'select2';
 
 const initSelect2 = () => {
-  $('.select2').select2();
+  $('.select2').select2({
+    theme: "bootstrap"
+  });
 };
 
 export { initSelect2 };
