b3e4aa21c0a629f4af43d4c1b6840c7d78a8d9cf
Florin Diconescu <florin.diconescu@gmail.com>
2019-07-27 04:41:12 +0300

    adding select2 for the club selection

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,8 @@ import { initAutocompleteAddress } from '../plugins/autocomplete_address';
 import { initAutocompleteCity } from '../plugins/autocomplete_city';
 import { autoLoserSelection } from '../components/autoLoserSelection';
 import "../plugins/flatpickr";
+import 'select2/dist/css/select2.css';
+import { initSelect2 } from '../components/init_select2';
 
 const tabs = document.querySelectorAll(".tab-underlined");
 if (tabs) { tabs.forEach(addListener) };
@@ -11,3 +13,4 @@ if (tabs) { tabs.forEach(addListener) };
 initAutocompleteAddress();
 initAutocompleteCity();
 autoLoserSelection();
+initSelect2();
