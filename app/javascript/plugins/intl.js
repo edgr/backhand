8a36b36495c6abf3f40c3d1fdfc368b804f771e6
Florin Diconescu <florin.diconescu@gmail.com>
2019-07-29 00:42:26 +0300

    I think this way it should work

--- app/javascript/plugins/intl.js
+++ app/javascript/plugins/intl.js
@@ -1,5 +1,7 @@
 import intlTelInput from 'intl-tel-input';
 
 const input = document.querySelector("#user_phone_number");
-console.log(input);
-window.intlTelInput(input);
+if (input) {
+  intlTelInput(input);
+}
+
