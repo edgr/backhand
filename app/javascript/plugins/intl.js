d2a3dd990ec335b72dfcd436dd084f5ccb3b4ce4
Florin Diconescu <florin.diconescu@gmail.com>
2019-07-29 01:20:06 +0300

    display flags fixed

--- app/javascript/plugins/intl.js
+++ app/javascript/plugins/intl.js
@@ -2,6 +2,11 @@ import intlTelInput from 'intl-tel-input';
 
 const input = document.querySelector("#user_phone_number");
 if (input) {
-  intlTelInput(input);
+  intlTelInput(input, {
+    formatOnInit: true,
+    separateDialCode: true,
+    onlyCountries: ['at', 'be', 'bg', 'cz', 'dk', 'de', 'ee', 'ie', 'el', 'es', 'fr', 'hr', 'it', 'cy', 'lv', 'lt', 'lu', 'hu', 'mt', 'nl', 'pl', 'pt', 'ro', 'si', 'sk', 'fi', 'se', 'uk'],
+    initialCountry: "es",
+  });
 }
 
