e3953fdec52440af2e2053f9c58b8b76d3c395d6
Florin Diconescu <florin.diconescu@gmail.com>
2019-07-29 21:34:44 +0300

    validation works as it is supposed

--- app/javascript/plugins/intl.js
+++ app/javascript/plugins/intl.js
@@ -1,38 +1,48 @@
 import intlTelInput from 'intl-tel-input';
 
-const input = document.querySelector("#user_phone_number");
-const errorMsg = document.querySelector("#intl-error-msg");
-const validMsg = document.querySelector("#intl-valid-msg");
+const input = document.getElementById("user_phone_number");
+const errorMsg = document.getElementById("error-msg");
+const validMsg = document.getElementById("valid-msg");
 const errorMap = ["Invalid number", "Invalid country code", "Too short", "Too long", "Invalid number"];
 
-const iti = intlTelInput(input, {
-  formatOnInit: true,
-  separateDialCode: true,
-  autoPlaceholder: "aggressive",
-  preferredCountries: ['at', 'be', 'bg', 'cz', 'dk', 'de', 'ee', 'ie', 'el', 'es', 'fr', 'hr', 'it', 'cy', 'lv', 'lt', 'lu', 'hu', 'mt', 'nl', 'pl', 'pt', 'ro', 'si', 'sk', 'fi', 'se', 'uk'],
-  initialCountry: "es",
-});
+if (input) {
+  const iti = intlTelInput(input, {
+    // formatOnInit: true,
+    // separateDialCode: true,
+    autoPlaceholder: "aggressive",
+    preferredCountries: ['at', 'be', 'bg', 'cz', 'dk', 'de', 'ee', 'ie', 'el', 'es', 'fr', 'hr', 'it', 'cy', 'lv', 'lt', 'lu', 'hu', 'mt', 'nl', 'pl', 'pt', 'ro', 'si', 'sk', 'fi', 'se', 'uk'],
+    initialCountry: "es",
+    utilsScript: "../../build/js/utils.js?1562189064761"
+  });
 
-const reset = function() {
-  input.classList.remove("error");
-  errorMsg.innerHTML = "";
-  errorMsg.classList.add("hidden");
-  validMsg.classList.add("hidden");
-};
+  const reset = () => {
+    input.classList.remove("error");
+    errorMsg.innerHTML = "";
+    errorMsg.classList.add("hide");
+    validMsg.classList.add("hide");
+  }
 
-input.addEventListener('blur', function() {
-  reset();
-  if (input.value.trim()) {
-    if (iti.isValidNumber()) {
-    // do nothing if valid, let it pass
+  input.addEventListener('blur', () => {
+    reset();
+    if (input.value.trim()) {
+      if (iti.isValidNumber()) {
+        validMsg.classList.remove("hide");
     } else {
       input.classList.add("error");
-      var errorCode = iti.getValidationError();
+      const errorCode = iti.getValidationError();
       errorMsg.innerHTML = errorMap[errorCode];
-      errorMsg.classList.remove("hidden");
+      errorMsg.classList.remove("hide");
+      }
     }
-  }
-});
+  });
 
-input.addEventListener('change', reset);
-input.addEventListener('keyup', reset);
+  input.addEventListener('change', reset);
+  input.addEventListener('keyup', reset);
+}
+
+
+
+
+
+
+
