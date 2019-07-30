8e5f33c061deb429b038c57726495046421aa5c1
Florin Diconescu <florin.diconescu@gmail.com>
2019-07-30 19:31:12 +0300

    small update

--- app/javascript/components/changelabelpictureupload.js
+++ app/javascript/components/changelabelpictureupload.js
@@ -2,20 +2,11 @@ const label = document.querySelector('.custom-file-label');
 
 const changeLabelValue = () => {
   if (label) {
-    // const input = document.querySelector('custom-file-input')
     const file = document.getElementById('user_picture');
     file.addEventListener('change', () => {
       label.innerHTML = file.files[0].name
     });
   }
 }
-
-// const changeLabelValue = (event) => {
-//   const label = document.querySelector('.custom-file-label')
-//   const file = document.getElementById('user_picture').files[0]
-//   if (label && file) {
-//     label.innerHTML = file.name
-//   }
-// }
 
 export { changeLabelValue };
