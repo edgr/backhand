c7080b0215d4779c00dffcfabe39fd98c04de745
Edouard Garret <edouard.garret@gmail.com>
2019-08-02 12:48:05 +0200

    add f.association input type for selecting opponent

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -5,10 +5,10 @@ class MatchesController < ApplicationController
 
   def create
     @match = Match.new(match_params)
-    if @match.save
+    if @match.save!
       redirect_to matches_path
     else
-      render 'new'
+      render :new
     end
   end
 
