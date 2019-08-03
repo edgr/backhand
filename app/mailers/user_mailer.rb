96bee4e8ac16461214c21ae8b33bed012888486a
Edouard Garret <edouard.garret@gmail.com>
2019-08-03 10:08:46 +0200

    indent email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,6 +7,9 @@ class UserMailer < ApplicationMailer
   #
   def welcome
     @user = params[:user]
-    mail(to: @user.email, subject: 'Welcome to Backhand')
+    mail(
+      to: @user.email,
+      subject: 'Welcome to Backhand'
+      )
   end
 end
