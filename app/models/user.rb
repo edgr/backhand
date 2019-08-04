52ee498486d0af60323f7eb4811ea3ffde679318
Edouard Garret <edouard.garret@gmail.com>
2019-08-04 11:11:35 +0200

    adding uniqueness on phone number

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,7 @@ class User < ApplicationRecord
                     uniqueness: true,
                     format: { with: URI::MailTo::EMAIL_REGEXP, message: "only allows valid emails" }
   validates :phone_number, presence: true,
+                           uniqueness: true,
                            numericality: true,
                            length: { minimum: 8, maximum: 15 }
   validates :first_name, :last_name, presence: true,
