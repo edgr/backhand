0ba00e86f6ae8e171c189278a43eca001db2ffc6
Florin Diconescu <florin.diconescu@gmail.com>
2019-08-05 16:14:59 +0300

    fix tables dependency in the models

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,7 +1,9 @@
 class Match < ApplicationRecord
-  belongs_to :player, class_name: 'User'
-  belongs_to :opponent, class_name: 'User'
+  belongs_to :player_1, class_name: 'User'
+  belongs_to :player_2, class_name: 'User'
   has_many :user_reviews, dependent: :destroy
+  has_many :match_sets, dependent: :destroy
+  has_one :match_result, dependent: :destroy
 
-  validates :player, :opponent, :score, :date, :location, :winner, :loser, presence: :true
+  validates :player_1, :player_2, :date, presence: :true
 end
