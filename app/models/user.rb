55fc17d3ef031220520a9ba3268853a6dd7e4d4d
Edouard Garret <edouard.garret@gmail.com>
2019-08-10 13:30:28 +0200

    adding club optional in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
   has_many :sent_reviews, class_name: 'UserReview', foreign_key: :sender_id, dependent: :destroy
   has_many :received_reviews, class_name: 'UserReview', foreign_key: :receiver_id, dependent: :destroy
 
-  belongs_to :club
+  belongs_to :club, optional: true
 
   # has_many :sent_challenges, class_name: 'Challenge', foreign_key: :challenger_id, dependent: :destroy
   # has_many :received_challenges, class_name: 'Challenge', foreign_key: :challengee_id, dependent: :destroy
