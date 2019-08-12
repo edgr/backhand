7f11844b0e9da099478a4addccec80defc48fa4e
Florin Diconescu <florin.diconescu@gmail.com>
2019-08-12 17:52:28 +0200

    adding some js validation for the first review step, so the form won't crash

--- app/javascript/components/review_steps.js
+++ app/javascript/components/review_steps.js
@@ -5,12 +5,24 @@ const nextStep = document.getElementById('next-review-step');
 const secondStep = document.querySelector('.user-review-step2');
 const backStep = document.getElementById('back-review-step');
 
+const notReally = document.getElementById('user_review_thumb_false');
+const ofCourse = document.getElementById('user_review_thumb_true');
+const reviewContent = document.getElementById('user_review_content');
+
 const reviewSteps = () => {
   if (firstStep) {
     secondStep.style.display = 'none';
     nextStep.addEventListener('click', () => {
-      firstStep.style.display = 'none';
-      secondStep.style.display = '';
+      if (notReally.checked || ofCourse.checked) {
+        if (reviewContent.value) {
+          firstStep.style.display = 'none';
+          secondStep.style.display = '';
+        } else {
+          swal('Please write something about your partner!')
+        }
+      } else {
+      swal('Please rate your partner!')
+      }
     });
     backStep.addEventListener('click', () => {
       secondStep.style.display = 'none';
