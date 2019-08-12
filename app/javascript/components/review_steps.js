e4d1dcb962869371641d6b8b261b9e8536097d81
Florin Diconescu <florin.diconescu@gmail.com>
2019-08-12 16:46:38 +0200

    breaking the review form into 2 steps

---
+++ app/javascript/components/review_steps.js
@@ -0,0 +1,20 @@
+const firstStep = document.querySelector('.user-review-step1');
+const nextStep = document.getElementById('next-review-step');
+const secondStep = document.querySelector('.user-review-step2');
+const backStep = document.getElementById('back-review-step');
+
+const reviewSteps = () => {
+  if (firstStep) {
+    secondStep.style.display = 'none';
+    nextStep.addEventListener('click', () => {
+      firstStep.style.display = 'none';
+      secondStep.style.display = '';
+    });
+    backStep.addEventListener('click', () => {
+      secondStep.style.display = 'none';
+      firstStep.style.display = '';
+    });
+  }
+}
+
+export { reviewSteps };
