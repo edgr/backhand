a8abb6487a4becf716db387627852b8b270566c2
Florin Diconescu <florin.diconescu@gmail.com>
2019-08-16 01:50:40 +0200

    small update on js so it won't crash on other pages

--- app/javascript/components/select_opponent.js
+++ app/javascript/components/select_opponent.js
@@ -1,25 +1,23 @@
 const selectPlayer2 = document.getElementById('match_player_2');
 const selectWinner = document.getElementById('match_result_winner');
 
-const opponentName = document.getElementById('player_2_name');
-const playerDivs = document.querySelectorAll('.avatar-name-match-record');
-const opponentPicture = playerDivs[1].firstElementChild;
-
 
 const myFunction = (event) => {
   if (selectPlayer2) {
     selectWinner.lastChild.remove();
-    opponentName.innerText = "";
 
     const i = event.currentTarget.selectedIndex;
     const opponentOption = `<option value="${event.currentTarget.value}">${event.currentTarget.options[i].innerText}</option>`;
     selectWinner.insertAdjacentHTML('beforeend', opponentOption);
 
     const pictureLink = event.currentTarget.options[i].dataset.picture;
+    const playerDivs = document.querySelectorAll('.avatar-name-match-record');
+    const opponentPicture = playerDivs[1].firstElementChild;
     opponentPicture.src = pictureLink;
 
-    const opponentNameString = `${event.currentTarget.options[i].innerText}`;
-    opponentName.insertAdjacentHTML('beforeend', opponentNameString);
+    const opponentNameString = event.currentTarget.options[i].innerText;
+    const opponentName = document.getElementById('player_2_name');
+    opponentName.innerText = opponentNameString;
   }
 }
 
