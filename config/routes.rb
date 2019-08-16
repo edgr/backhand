9c6f02b475154cf55abebb7e629216645bc433f5
Florin Diconescu <florin.diconescu@gmail.com>
2019-08-16 22:17:18 +0200

    making some routes for the matches and fix the sql query not to break on the matches index

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,14 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" }
   resources :user_steps
 
-  resources :matches, only: [:show, :index]
-
   patch '/match_results/:id/confirm', to: 'match_results#confirm', as: 'confirm'
 
   resources :users, only: [ :index, :show, :edit, :update ] do
     resources :user_reviews, only: [:new, :create, :destroy]
-    resources :matches, only: [:new, :create, :show, :edit, :update]
-    get 'matches', to: 'matches#player_matches'
+    get '/my_matches', to: 'matches#player_matches', as: 'my_matches'
   end
+
+  resources :matches, only: [:index, :show, :new, :create, :edit, :update]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
