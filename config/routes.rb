0346a226f7cf18c3f2062c527240588fbf6fdb47
Edouard Garret <edouard.garret@gmail.com>
2019-08-16 12:26:37 +0200

    removing commented out routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" }
   resources :user_steps
 
-  # resources :sign_ups
-  # resources :finish_profiles
-
-  # resources :challenges, only: [:index, :show, :update] do
-  # end
-
   resources :matches, only: [:new, :create, :show, :index, :edit, :update]
 
   patch '/match_results/:id/confirm', to: 'match_results#confirm', as: 'confirm'
@@ -17,13 +11,6 @@ Rails.application.routes.draw do
    resources :users, only: [ :index, :show, :edit, :update ] do
     resources :user_reviews, only: [:new, :create, :destroy]
   end
-
-  # post 'challenges', to:'challenges#create', as: :create_challenge
-
-  # patch 'challenges/:id/accept', to: 'challenges#accept', as: :accept_challenge
-  # patch 'challenges/:id/decline', to: 'challenges#decline', as: :decline_challenge
-  # patch 'challenges/:id/cancel', to: 'challenges#cancel', as: :cancel_challenge
-  # patch 'challenges/:id/played', to: 'challenges#played', as: :played_challenge
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
