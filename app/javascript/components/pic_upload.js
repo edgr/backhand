102fa2945a4fb84a1a61e5ed8d5c059dfd9b6ed8
Edouard Garret <edouard.garret@gmail.com>
2019-08-21 16:37:52 +0200

    still not working

--- app/javascript/components/pic_upload.js
+++ app/javascript/components/pic_upload.js
@@ -1,18 +1,21 @@
 import FileUploadWithPreview from 'file-upload-with-preview'
 import 'file-upload-with-preview/dist/file-upload-with-preview.min.css'
 
-const photo = new FileUploadWithPreview('myUniqueUploadId');
-
-const getPicture = (event) => {
-  if (photo) {
-    photo.cachedFileArray[0];
-  }
-}
-
 const picUpload = () => {
+  const photo = new FileUploadWithPreview('user_picture');
   const preview = document.getElementById('user_picture');
   if (preview) {
-    preview.addEventListener('fileUploadWithPreview:imagesAdded', getPicture());
+    window.addEventListener('fileUploadWithPreview:imagesAdded', function(e) {
+      // e.detail.uploadId
+      // e.detail.cachedFileArray
+      // e.detail.addedFilesCount
+      // Use e.detail.uploadId to match up to your specific input
+      if (e.detail.uploadId === 'user_picture') {
+        const pic = e.detail.cachedFileArray[0];
+        const playerPic = JSON.stringify(pic);
+        console.log(playerPic);
+      }
+    });
   }
 };
 
