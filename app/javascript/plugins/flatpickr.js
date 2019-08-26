531c66821c801d9b4aced98e9ee55fb0d8f628bf
Florin Diconescu <florin.diconescu@gmail.com>
2019-08-26 17:24:09 +0200

    small fix on signup form to improve ux

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,17 +1,25 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/themes/material_blue.css';
 
-flatpickr(".datepicker", {
-  altInput: true,
-  disableMobile: true,
-  altFormat: "j F Y",
-  dateFormat: "d-m-Y",
-})
+const signupPicker = document.querySelector(".datepicker-signup");
 
-flatpickr(".datepicker-match", {
-  altInput: true,
-  disableMobile: true,
-  altFormat: "j F Y",
-  dateFormat: "d-m-Y",
-  defaultDate: "today"
-})
+if (signupPicker) {
+  flatpickr(".datepicker-signup", {
+    altInput: true,
+    altFormat: "j F Y",
+    dateFormat: "d-m-Y",
+    defaultDate: "01-01-2000"
+  });
+}
+
+const matchPicker = document.querySelector(".datepicker-match");
+
+if (matchPicker) {
+  flatpickr(".datepicker-match", {
+    altInput: true,
+    disableMobile: true,
+    altFormat: "j F Y",
+    dateFormat: "d-m-Y",
+    defaultDate: "today"
+  });
+}
