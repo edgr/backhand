56c8503be3017b2b21422c46cbc1ea0cb12426a4
Florin Diconescu <florin.diconescu@gmail.com>
2019-08-28 00:26:21 +0200

    a smoll refacto

--- app/javascript/components/photo_preview.js
+++ app/javascript/components/photo_preview.js
@@ -1,20 +1,14 @@
 const photoPreview = () => {
   const uploadInput = document.querySelector(".custom-file-input");
-  const editInput = document.querySelector(".edit-file-input");
+  const edit = document.getElementById("edit-photo-form");
   const imagePreview = document.querySelector(".imagePreview");
+  if (edit) {
+    imagePreview.style.backgroundImage = `url(${imagePreview.dataset.picture})`
+  }
   if (uploadInput) {
     uploadInput.addEventListener('change', () => {
       const fReader = new FileReader();
       fReader.readAsDataURL(uploadInput.files[0]);
-      fReader.addEventListener('loadend', (event) => {
-        imagePreview.style.backgroundImage = `url("${event.target.result}")`;
-      });
-    });
-  } else if (editInput) {
-    imagePreview.style.backgroundImage = `url(${imagePreview.dataset.picture})`
-    editInput.addEventListener('change', () => {
-      const fReader = new FileReader();
-      fReader.readAsDataURL(editInput.files[0]);
       fReader.addEventListener('loadend', (event) => {
         imagePreview.style.backgroundImage = `url("${event.target.result}")`;
       });
