1703eea21a70595fc14e5e2f66c10b21fd941379
Florin Diconescu <florin.diconescu@gmail.com>
2019-08-29 10:46:38 +0200

    just made the photo stay in an upward position on the client side

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,3 +1,24 @@
 class PictureUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
+
+  version :rotated_90 do
+    eager
+    cloudinary_transformation transformation: [
+      { angle: 90 }
+    ]
+  end
+
+  version :rotated_180 do
+    eager
+    cloudinary_transformation transformation: [
+      { angle: 180 }
+    ]
+  end
+
+  version :rotated_270 do
+    eager
+    cloudinary_transformation transformation: [
+      { angle: 270 }
+    ]
+  end
 end
