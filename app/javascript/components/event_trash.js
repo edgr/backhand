5abfdbfb2acfd56bfa47ba9439829d7383cda80c
Edouard Garret <edouard.garret@gmail.com>
2019-10-04 23:16:46 +0200

    adding if condition to js

--- app/javascript/components/event_trash.js
+++ app/javascript/components/event_trash.js
@@ -1,15 +1,17 @@
 const eventCards = document.querySelectorAll('.card-and-actions')
 
 const showTrash = () => {
-  eventCards.forEach((eventCard) => {
-    const card = eventCard.querySelector('.event-card');
-    const trash = eventCard.querySelector('.card-delete');
-    const date = eventCard.querySelector('.date-club')
-    card.addEventListener('click', (event) => {
-      trash.classList.toggle('d-none');
-      date.classList.toggle('d-none');
+  if (eventCards) {
+    eventCards.forEach((eventCard) => {
+      const card = eventCard.querySelector('.event-card');
+      const trash = eventCard.querySelector('.card-delete');
+      const date = eventCard.querySelector('.date-club')
+      card.addEventListener('click', (event) => {
+        trash.classList.toggle('d-none');
+        date.classList.toggle('d-none');
+      })
     })
-  })
+  }
 }
 
 export { showTrash }
