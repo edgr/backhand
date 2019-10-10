fafe311ddeaf5e34f77af5441138a9ed17deb3d8
Florin Diconescu <florin.diconescu@gmail.com>
2019-10-10 17:40:10 +0200

    remove password requirement on profile update

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,4 +5,8 @@ class RegistrationsController < Devise::RegistrationsController
   def after_sign_up_path_for(resource)
     user_steps_path
   end
+
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
 end
