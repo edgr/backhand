2f429e0b38d8d0a52282e2c030afed51ef46b974
Edouard Garret <edouard.garret@gmail.com>
2019-12-04 10:12:33 +0800

    adding preview path for action mailer preview in dev environment

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -8,4 +8,23 @@ class UserMailerPreview < ActionMailer::Preview
     UserMailer.with(user: user).welcome
   end
 
+  def new_game
+    inviter = User.first
+    partner = User.last
+    event = Event.first
+
+    self.template_model = {
+      inviter_first_name: inviter.first_name,
+      partner_first_name: partner.first_name,
+      date: event.date_time.strftime("%A %b %e"),
+      time: event.date_time.strftime("%l:%M%p"),
+      location: event.club.name
+    }
+    # mail to: partner.email
+
+    # UserMailer.with(user: user).welcome
+    UserMailer.with(inviter: inviter, partner: partner, event: event).new_game.deliver_now
+
+  end
+
 end
