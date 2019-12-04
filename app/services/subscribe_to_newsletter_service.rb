628473328c409d229c138d4fda617104696f38a4
Edouard Garret <edouard.garret@gmail.com>
2019-12-04 17:23:24 +0800

    remove merge fields as user does not have them yet

--- app/services/subscribe_to_newsletter_service.rb
+++ app/services/subscribe_to_newsletter_service.rb
@@ -9,11 +9,11 @@ class SubscribeToNewsletterService
     @gibbon.lists(@audience_id).members.create(
       body: {
         email_address: @user.email,
-        status: "subscribed",
-        merge_fields: {
-          FNAME: @user.first_name,
-          LNAME: @user.last_name
-        }
+        status: "subscribed"
+        # merge_fields: {
+        #   FNAME: @user.first_name,
+        #   LNAME: @user.last_name
+        # }
       }
     )
   end
