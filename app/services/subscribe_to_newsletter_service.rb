0180e069ebfda8dd5b6f59c4257e6aba427a9a18
Edouard Garret <edouard.garret@gmail.com>
2019-12-04 17:32:27 +0800

    really remove them

--- app/services/subscribe_to_newsletter_service.rb
+++ app/services/subscribe_to_newsletter_service.rb
@@ -10,10 +10,6 @@ class SubscribeToNewsletterService
       body: {
         email_address: @user.email,
         status: "subscribed"
-        # merge_fields: {
-        #   FNAME: @user.first_name,
-        #   LNAME: @user.last_name
-        # }
       }
     )
   end
