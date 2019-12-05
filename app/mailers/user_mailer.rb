b533bbaebed298ced6557bcc396d70956cea1401
Edouard Garret <edouard.garret@gmail.com>
2019-12-05 08:20:38 +0800

    adding player review mailer and preview using Postmark

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,24 +9,44 @@ class UserMailer < ApplicationMailer
   #
 
   def welcome
-    user = params[:user]
+    @user = params[:user]
     mail(
-      to: user.email,
+      to: @user.email,
       bcc: 'edouard@backhandapp.com'
     )
   end
 
   def new_game
-    inviter = params[:inviter]
-    partner = params[:partner]
-    event = params[:event]
+    @inviter = params[:inviter]
+    @partner = params[:partner]
+    @event = params[:event]
     self.template_model = {
-      inviter_first_name: inviter.first_name,
-      partner_first_name: partner.first_name,
-      date: event.date_time.strftime("%A %b %e"),
-      time: event.date_time.strftime("%l:%M%p"),
-      location: event.club.name
+      inviter_first_name: @inviter.first_name,
+      partner_first_name: @partner.first_name,
+      date: @event.date_time.strftime("%A %b %e"),
+      time: @event.date_time.strftime("%l:%M%p"),
+      location: @event.club.name
     }
-    mail to: partner.email
+    mail to: @partner.email
+  end
+
+  def new_player_review
+    @sender = params[:sender]
+    @receiver = params[:receiver]
+    @user_review = params[:user_review]
+    @url = sender_url(@sender.id)
+    self.template_model = {
+      sender_first_name: @sender.first_name,
+      receiver_first_name: @receiver.first_name,
+      user_review_content: @user_review.content,
+      sender_url: @url
+    }
+    mail to: @receiver.email
+  end
+
+  private
+
+  def sender_url(id)
+    "https://www.backhandapp.com/users/#{id}/user_reviews/new"
   end
 end
--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -2,16 +2,33 @@
 class UserMailerPreview < ActionMailer::Preview
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/welcome
   def welcome
-    user = User.first
+    @user = User.first
 
-    UserMailer.with(user: user).welcome
+    UserMailer.with(user: @user).welcome
   end
 
   def new_game
-    inviter = User.first
-    partner = User.last
-    event = Event.first
+    @inviter = User.find(273)
+    @partner = User.find(274)
+    @event = Event.first
 
-    UserMailer.with(inviter: inviter, partner: partner, event: event).new_game.deliver_now
+    UserMailer.with(
+      inviter: @inviter,
+      partner: @partner,
+      event: @event
+    ).new_game.deliver_now
   end
+
+  def new_player_review
+    @sender = User.find(273)
+    @receiver = User.find(274)
+    @user_review = UserReview.first
+
+    UserMailer.with(
+      sender: @sender,
+      receiver: @receiver,
+      user_review: @user_review
+    ).new_player_review.deliver_now
+  end
+
 end
