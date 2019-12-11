09f21e01a4d78565d4131cd4cfe8f75e488502b1
Edouard Garret <edouard.garret@gmail.com>
2019-12-11 19:59:10 +0800

    first steps in presenting the club on laptop and mobile, removed the about section

--- app/presenters/club_presenter.rb
+++ app/presenters/club_presenter.rb
@@ -9,6 +9,36 @@ class ClubPresenter < BasePresenter
     "#{club.courts.count} courts (#{courts_type(club.courts)})"
   end
 
+  def courts_indoors
+    if club.courts_indoors.count == 0
+      "no indoor courts"
+    elsif club.courts_indoors.count == 1
+      "1 indoor court"
+    else
+      "#{club.courts_indoors.count} indoor courts"
+    end
+  end
+
+  def courts_with_lights
+    if club.courts_with_lights.count == 0
+      "no courts with lights"
+    elsif club.courts_with_lights.count == 1
+      "1 court with lights"
+    else
+      "#{club.courts_with_lights.count} court(s) with lights"
+    end
+  end
+
+  def singles_courts
+    if club.singles_courts.count == 0
+      "no \'singles only\' with lights"
+    elsif club.singles_courts.count == 1
+      "1 \'singles only\' court"
+    else
+      "#{club.singles_courts.count} \"singles only\" court(s)"
+    end
+  end
+
   private
 
   def courts_type(courts)
