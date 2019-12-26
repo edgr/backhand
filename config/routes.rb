8b6566e28dcddfe63446e22fa5f75131a1e24f5f
Edouard Garret <edouard.garret@gmail.com>
2019-12-26 14:35:15 +0900

    add errors controller and modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  # get 'errors/not_found'
+  # get 'errors/internal_server_error'
   root to: 'pages#home'
   get '/welcome', to: 'pages#welcome', as: 'welcome'
+  match '/404', to: 'errors#not_found', via: :all
+  match '/500', to: 'errors#internal_server_error', via: :all
 
   devise_for :users, controllers: { registrations: "registrations" }
   resources :user_steps
