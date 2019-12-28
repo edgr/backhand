3f204caaea9adb5058797fbae7cd48bcf8d1ea3a
Edouard Garret <edouard.garret@gmail.com>
2019-12-28 16:26:28 +0900

    set user language through json field, added around_action in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,15 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_raven_context
+  around_action :switch_locale
 
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
+  def switch_locale(&action)
+    locale = current_user.settings[:language] || I18n.default_locale
+    I18n.with_locale(locale, &action)
   end
 
   private
@@ -32,7 +38,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:phone_number])
     # devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :level, :address, :phone_number, :picture, :style_of_play, :handedness, :backhand_style, :gender, :height, :weight, :birthday, :place_of_birth, :country, :bio, :club_id, :angle], settings: {})
     devise_parameter_sanitizer.permit(:account_update) do |user|
-      user.permit(:first_name, :last_name, :level, :address, :phone_number, :picture, :style_of_play, :handedness, :backhand_style, :gender, :height, :weight, :birthday, :place_of_birth, :country, :bio, :club_id, :angle, :new_shoutout_email, :new_game_event_email, :new_match_result_email, :new_player_review_email, :confirmed_match_result_email)
+      user.permit(:first_name, :last_name, :level, :address, :phone_number, :picture, :style_of_play, :handedness, :backhand_style, :gender, :height, :weight, :birthday, :place_of_birth, :country, :bio, :club_id, :angle, :new_shoutout_email, :new_game_event_email, :new_match_result_email, :new_player_review_email, :confirmed_match_result_email, :language)
     end
   end
 
