17a621906679d982cc36b5307acf361a3104f35d
Edouard Garret <edouard.garret@gmail.com>
2019-12-28 18:04:57 +0900

    started fr translations, refacto switch locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   end
 
   def switch_locale(&action)
-    locale = current_user.settings[:language] || I18n.default_locale
+    locale = current_user.present? ? current_user.settings[:language] : I18n.default_locale
     I18n.with_locale(locale, &action)
   end
 
@@ -36,7 +36,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:phone_number])
-    # devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :level, :address, :phone_number, :picture, :style_of_play, :handedness, :backhand_style, :gender, :height, :weight, :birthday, :place_of_birth, :country, :bio, :club_id, :angle], settings: {})
     devise_parameter_sanitizer.permit(:account_update) do |user|
       user.permit(:first_name, :last_name, :level, :address, :phone_number, :picture, :style_of_play, :handedness, :backhand_style, :gender, :height, :weight, :birthday, :place_of_birth, :country, :bio, :club_id, :angle, :new_shoutout_email, :new_game_event_email, :new_match_result_email, :new_player_review_email, :confirmed_match_result_email, :language)
     end
