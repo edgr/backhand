5c97d2c4cbbc1fd876a5bf7a6ce6a8084d5952b6
Edouard Garret <edouard.garret@gmail.com>
2020-01-08 21:09:53 +0700

    adding option to hide Whatsapp number

--- app/javascript/components/actions_button.js
+++ app/javascript/components/actions_button.js
@@ -6,9 +6,21 @@ const openActions = () => {
     const linkButton = document.getElementById('copy-link-button')
     const innerIcon = moreButton.querySelector('.mdc-fab__icon')
     moreButton.addEventListener('click', (event) => {
-      reviewButton.classList.toggle('hidden');
-      whatsappButton.classList.toggle('hidden');
-      linkButton.classList.toggle('hidden');
+      if (reviewButton) {
+        reviewButton.classList.toggle('hidden');
+      }
+      if (whatsappButton){
+        whatsappButton.classList.toggle('hidden');
+        if (linkButton) {
+          linkButton.classList.toggle('hidden');
+        }
+      } else {
+        if (linkButton) {
+          linkButton.classList.toggle('hidden');
+          linkButton.classList.toggle('b-230');
+          linkButton.classList.toggle('b-160');
+        }
+      }
       if (innerIcon.innerText === 'add') {
         innerIcon.innerText = 'close'
       }
