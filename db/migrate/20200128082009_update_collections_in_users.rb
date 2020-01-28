b4c814b0d048d2d83be90859cc7ee2050a4ecd87
Edouard Garret <edouard.garret@gmail.com>
2020-01-28 21:25:06 +0100

    moved everything to first migration file

--- db/migrate/20200128082009_update_collections_in_users.rb
+++ db/migrate/20200128082009_update_collections_in_users.rb
@@ -4,6 +4,16 @@ class UpdateCollectionsInUsers < ActiveRecord::Migration[5.2]
       user.gender = user.gender.downcase
       user.handedness = user.handedness.downcase
       user.backhand_style = user.backhand_style.gsub(' ', '_')
+      if user.level == "Semi-pro +"
+        user.level = user.level.gsub('-', '_').gsub(' +', '_plus').downcase
+      elsif user.level == "Semi-pro"
+        user.level = user.level.gsub('-', '_').downcase
+      else
+        user.level = user.level.gsub(' +', '_plus').downcase
+      end
+      if user.style_of_play == "server-volleyer"
+        user.style_of_play = "server_volleyer"
+      end
       user.save!
     end
   end
