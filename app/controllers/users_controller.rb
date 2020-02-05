f04a87f3142b1b3f6390b5a0d22b0e7a7d136b66
Florin Diconescu <florin.diconescu@gmail.com>
2020-02-05 10:33:24 +0100

    updated pagination, to make it work on filters too

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,39 +5,32 @@ class UsersController < ApplicationController
   def index
     if params[:search]
       @query = format_query(params[:search])
-      @users = SearchUsers.new(params[:search]).call
+      @users = SearchUsers.new(params[:search]).call.ordered_by_points.paginate(page: params[:page], per_page: 20)
     else
       @users = User.active.ordered_by_points
       update_ranking
+      @users = @users.paginate(page: params[:page], per_page: 20)
     end
-    respond_to do |format|
-      format.html
-      format.js
-    end
-    @users = @users.ordered_by_points.paginate(page: params[:page], per_page: 12)
+    respond_to :html, :js
   end
 
   def show
     @user = User.find(params[:id])
     @reviews = UserReview.where(receiver: @user)
     @played_matches = @user.matches_won + @user.matches_lost
-    if @user.present?
-      unless @played_matches.count == 0
-        @progressbar = ((@user.matches_won.count / @played_matches.count.to_f) * 100).to_i
-      end
-    end
+    @progressbar = calculate_progrssbar(@user) if @user.present? && @played_matches.size.positive?
   end
 
   def rankings
-    @users = User.active
+    @users = User.active.ordered_by_points
     update_ranking
-    @users = @users.ordered_by_points
   end
 
   private
 
   def format_query(params)
-    return unless params[:query].present?
+    return
+    unless params[:query].present?
     set_query_indication
     params[:query] = params[:query].reject do |value|
       value.blank?
@@ -50,7 +43,11 @@ class UsersController < ApplicationController
   end
 
   def update_ranking
-    @users = @users.ordered_by_points
     @users.each_with_index { |user, index| user.update(ranking: index + 1) }
+  end
+
+  def calculate_progrssbar(user)
+    played_matches = user.matches_won + user.matches_lost
+    ((user.matches_won.count / played_matches.count.to_f) * 100).to_i
   end
 end
