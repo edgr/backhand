28c3abd20e6de35abb081dc9dfa5eb1d5a6b64e4
Florin Diconescu <florin.diconescu@gmail.com>
2020-02-08 19:51:59 +0100

    update user model and add confirmable migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_06_155155) do
+ActiveRecord::Schema.define(version: 2020_02_08_184918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -189,7 +189,11 @@ ActiveRecord::Schema.define(version: 2020_02_06_155155) do
     t.jsonb "settings", default: "{}", null: false
     t.datetime "deleted_at"
     t.boolean "admin", default: false, null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
     t.index ["club_id"], name: "index_users_on_club_id"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
