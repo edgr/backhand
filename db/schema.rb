5e71dd0102120e3c7d521077f0292509a4503239
Florin Diconescu <florin.diconescu@gmail.com>
2020-02-08 20:21:35 +0100

    updated migration in order to have reconfirmable

--- db/schema.rb
+++ db/schema.rb
@@ -192,6 +192,7 @@ ActiveRecord::Schema.define(version: 2020_02_08_184918) do
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["club_id"], name: "index_users_on_club_id"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
