f4beb9638b6d40f94a9261847dcf5e1623d891a9
Florin Diconescu <florin.diconescu@gmail.com>
2020-02-09 18:58:01 +0100

    fixed

--- app/javascript/plugins/scroll_last_message.js
+++
@@ -1,5 +0,0 @@
-const scrollLastMessageIntoView = () => {
-  const messages = document.querySelectorAll('.message');
-  const lastMessage = messages[messages.length - 1];
-  lastMessage.scrollIntoView();
-}
