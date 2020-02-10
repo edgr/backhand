68a36018fa7fedbb05d8daf75d808bc713d6f330
Edouard Garret <edouard.garret@gmail.com>
2020-02-10 21:59:00 +0100

    updated weird method hahaha

--- app/services/subscribe_to_newsletter_service.rb
+++ app/services/subscribe_to_newsletter_service.rb
@@ -6,7 +6,7 @@ class SubscribeToNewsletterService
   end
 
   def call
-    @gibbon.lists(@audience_id).members(lower_case_md5_hashed_email_address).upsert(
+    @gibbon.lists(@audience_id).members(Digest::MD5.hexdigest(@user.email.downcase)).upsert(
       body: {
         email_address: @user.email,
         status: "subscribed"
